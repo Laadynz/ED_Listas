f20694f951e132d39d3fbb74bdc03fad515bd63c
LadyNunez <laadynz@hotmail.com>
2022-04-09 14:26:38 -0700

    Buscar Anterior

--- ListasSimplementeLigadas/Lista.cs
+++ ListasSimplementeLigadas/Lista.cs
@@ -14,51 +14,98 @@ namespace ListasSimplementeLigadas
         {
             nodoInicial = new Nodo();
         }
-        public bool ValidaVacio() 
+        public bool ValidaVacio()
         {
             return (nodoInicial.Enlace == null);
         }
-        public void Vaciar() 
+        public void Vaciar()
         {
             nodoInicial.Enlace = null;
         }
-        public string RecorrerLista() 
+        public string RecorrerLista()
         {
             string datosLista = "";
             nodoActual = nodoInicial;
-            while (nodoActual.Enlace != null) 
+            while (nodoActual.Enlace != null)
             {
                 nodoActual = nodoActual.Enlace;
                 datosLista += $"{nodoActual.Valor}\n";
             }
             return datosLista;
         }
-        public void AgregarNodo(string dato) 
+        public void AgregarNodo(string dato)
         {
             nodoActual = nodoInicial;
-            while (nodoActual.Enlace != null) 
+            while (nodoActual.Enlace != null)
             {
                 nodoActual = nodoActual.Enlace;
             }
             Nodo nodoNuevo = new Nodo(dato);
             nodoActual.Enlace = nodoNuevo;
         }
-        public Nodo Buscar(string dato) 
+        public Nodo Buscar(string dato)
         {
-            if (ValidaVacio() == false) 
+            if (ValidaVacio() == false)
             {
                 Nodo nodoBusqueda = nodoInicial;
-                while (nodoBusqueda.Enlace != null) 
+                while (nodoBusqueda.Enlace != null)
                 {
                     nodoBusqueda = nodoBusqueda.Enlace;
-                    if (nodoBusqueda.Valor == dato) 
+                    if (nodoBusqueda.Valor == dato)
                     {
                         return nodoBusqueda;
                     }
                 }
-
             }
             return null;
         }
+        public Nodo BuscarPorIndice(int indice)
+        {
+            int Indice = -1;
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Enlace != null)
+                {
+                    nodoBusqueda = nodoBusqueda.Enlace;
+                    Indice++;
+                    if (Indice == indice)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+        public Nodo BuscarAnterior(string dato)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Enlace != null && nodoBusqueda.Enlace.Valor != dato)
+                {
+                    nodoBusqueda = nodoBusqueda.Enlace;
+                    if (nodoBusqueda.Enlace.Valor == dato) 
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+        public void BorrarNodo(string dato) 
+        {
+            if (ValidaVacio() == false) 
+            {
+                nodoActual = Buscar(dato);
+                if (nodoActual != null) 
+                {
+                    Nodo nodoAnterior = BuscarAnterior(dato);
+                    nodoAnterior.Enlace = nodoActual.Enlace;
+                    nodoActual.Enlace = null;
+                }
+            }
+        } 
+
     }
 }
