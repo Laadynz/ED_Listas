52fba957ef7b8859a595189e7f25d35e1c67605f
LadyNunez <laadynz@hotmail.com>
2022-04-23 13:08:49 -0700

    Agregando nuevo proyecto (Listas doblemente ligadas)

--- ListasSimplementeLigadas/Program.cs
+++ ListasSimplementeLigadas/Program.cs
@@ -1,13 +1,15 @@
 ﻿using ListasSimplementeLigadas;
 
 Lista listas = new Lista();
+listas.AgregarNodo("Cero");
 listas.AgregarNodo("Uno");
 listas.AgregarNodo("Dos");
-listas.AgregarNodo("Tres");
-Console.WriteLine(listas.RecorrerLista());
-Console.WriteLine("Buscando nodo Dos");
-Nodo nodoDos = listas.Buscar("Dos");
-Console.WriteLine(nodoDos.Valor);
-Console.WriteLine("Borrando Dos");
-listas.BorrarNodo("Dos");
-Console.WriteLine(listas.RecorrerLista());
+listas.AgregarNodo("Ocho");
+Nodo nodoBusqueda = listas.BuscarAnterior("Dos");
+Console.WriteLine(nodoBusqueda.Valor);
+//Console.WriteLine("Buscando nodo Dos");
+//Nodo nodoDos = listas.Buscar("Dos");
+//Console.WriteLine(nodoDos.Valor);
+//Console.WriteLine("Borrando Dos");
+//listas.BorrarNodo("Dos");
+//Console.WriteLine(listas.RecorrerLista());
